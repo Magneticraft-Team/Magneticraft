bc7d33c56274575a0576b8c1bd1b98c673b48702
Cypher121 <ds98s3a@gmail.com>
2016-04-17 20:18:10 -0700

    Redirect coordinate-based render to Vector-based one for TESR.

--- src/main/java/com/cout970/magneticraft/client/tilerender/TileRenderer.java
+++ src/main/java/com/cout970/magneticraft/client/tilerender/TileRenderer.java
@@ -1,12 +1,17 @@
 package com.cout970.magneticraft.client.tilerender;
 
 import net.darkaqua.blacksmith.render.tile.TileEntityRenderer;
+import net.darkaqua.blacksmith.vectors.Vect3d;
 import net.minecraft.tileentity.TileEntity;
 
 /**
  * Created by cout970 on 19/03/2016.
  */
 public abstract class TileRenderer<T extends TileEntity> extends TileEntityRenderer<T> {
+    @Override
+    public void renderTileEntityAt(T te, double x, double y, double z, float partialTicks, int destroyStage) {
+        renderTileEntity(te, new Vect3d(x, y, z), partialTicks, destroyStage);
+    }
 
     public abstract void initModels();
 }
