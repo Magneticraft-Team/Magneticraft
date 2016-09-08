85ae968be24523c60e2b23c883ea2fdde1fef94c
cout970 <thecout970@gmail.com>
2016-09-08 23:00:11 +0200

    small tweak for itemblocks
    Signed-off-by: cout970 <thecout970@gmail.com>

--- src/main/kotlin/com/cout970/magneticraft/block/itemblock/ItemBlockElectricPole.kt
+++ src/main/kotlin/com/cout970/magneticraft/block/itemblock/ItemBlockElectricPole.kt
@@ -3,10 +3,7 @@ package com.cout970.magneticraft.block.itemblock
 import com.cout970.magneticraft.block.BlockElectricPole
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
-import net.minecraft.util.EnumActionResult
 import net.minecraft.util.EnumFacing
-import net.minecraft.util.EnumHand
-import net.minecraft.util.SoundCategory
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
@@ -15,32 +12,6 @@ import net.minecraft.world.World
  */
 class ItemBlockElectricPole : ItemBlockBase(BlockElectricPole) {
 
-    override fun onItemUse(stack: ItemStack, playerIn: EntityPlayer, worldIn: World, p: BlockPos, hand: EnumHand, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float): EnumActionResult {
-        var pos = p
-        val iblockstate = worldIn.getBlockState(pos)
-        val block = iblockstate.block
-
-        if (!block.isReplaceable(worldIn, pos)) {
-            pos = pos.offset(facing)
-        }
-
-        if (stack.stackSize != 0 && playerIn.canPlayerEdit(pos, facing, stack)
-                && worldIn.canBlockBePlaced(this.block, pos, false, facing, null, stack)
-                && worldIn.canBlockBePlaced(this.block, pos.offset(EnumFacing.UP, 1), false, facing, null, stack)
-                && worldIn.canBlockBePlaced(this.block, pos.offset(EnumFacing.UP, 2), false, facing, null, stack)
-                && worldIn.canBlockBePlaced(this.block, pos.offset(EnumFacing.UP, 3), false, facing, null, stack)
-                && worldIn.canBlockBePlaced(this.block, pos.offset(EnumFacing.UP, 4), false, facing, null, stack)) {
-            val i = this.getMetadata(stack.metadata)
-            val iblockstate1 = this.block.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, i, playerIn)
-
-            if (placeBlockAt(stack, playerIn, worldIn, pos, facing, hitX, hitY, hitZ, iblockstate1)) {
-                val soundtype = this.block.soundType
-                worldIn.playSound(playerIn, pos, soundtype.placeSound, SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0f) / 2.0f, soundtype.getPitch() * 0.8f)
-                stack.stackSize--
-            }
-            return EnumActionResult.SUCCESS
-        } else {
-            return EnumActionResult.PASS
-        }
-    }
+    override fun getBlocksToPlace(world: World, pos: BlockPos, facing: EnumFacing, player: EntityPlayer, stack: ItemStack): List<BlockPos> = listOf(pos,
+            pos.offset(EnumFacing.UP, 1), pos.offset(EnumFacing.UP, 2), pos.offset(EnumFacing.UP, 3), pos.offset(EnumFacing.UP, 4))
 }
--- src/main/kotlin/com/cout970/magneticraft/block/itemblock/ItemBlockFeedingTrough.kt
+++ src/main/kotlin/com/cout970/magneticraft/block/itemblock/ItemBlockFeedingTrough.kt
@@ -3,10 +3,7 @@ package com.cout970.magneticraft.block.itemblock
 import com.cout970.magneticraft.block.BlockFeedingTrough
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
-import net.minecraft.util.EnumActionResult
 import net.minecraft.util.EnumFacing
-import net.minecraft.util.EnumHand
-import net.minecraft.util.SoundCategory
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
@@ -15,33 +12,6 @@ import net.minecraft.world.World
  */
 class ItemBlockFeedingTrough() : ItemBlockBase(BlockFeedingTrough) {
 
-    /**
-     * This method is override to avoid removing blocks when the feeding trough cannot be placed, this
-     * happens because this method only checks the center block on the trough
-     */
-    override fun onItemUse(stack: ItemStack, playerIn: EntityPlayer, worldIn: World, p: BlockPos, hand: EnumHand, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float): EnumActionResult {
-        var pos = p
-        val iblockstate = worldIn.getBlockState(pos)
-        val block = iblockstate.block
-
-        if (!block.isReplaceable(worldIn, pos)) {
-            pos = pos.offset(facing)
-        }
-
-        if (stack.stackSize != 0 && playerIn.canPlayerEdit(pos, facing, stack)
-            && worldIn.canBlockBePlaced(this.block, pos, false, facing, null, stack)
-            && worldIn.canBlockBePlaced(this.block, pos.add(playerIn.adjustedHorizontalFacing.directionVec), false, facing, null, stack)) {
-            val i = this.getMetadata(stack.metadata)
-            val iblockstate1 = this.block.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, i, playerIn)
-
-            if (placeBlockAt(stack, playerIn, worldIn, pos, facing, hitX, hitY, hitZ, iblockstate1)) {
-                val soundtype = this.block.soundType
-                worldIn.playSound(playerIn, pos, soundtype.placeSound, SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0f) / 2.0f, soundtype.getPitch() * 0.8f)
-                stack.stackSize--
-            }
-            return EnumActionResult.SUCCESS
-        } else {
-            return EnumActionResult.PASS
-        }
-    }
+    override fun getBlocksToPlace(world: World, pos: BlockPos, facing: EnumFacing, player: EntityPlayer, stack: ItemStack): List<BlockPos> =
+            listOf(pos, pos.add(player.adjustedHorizontalFacing.directionVec))
 }
