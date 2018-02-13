867000d4bc056f705a37ad3cb7b99bbcb5f8f013
cout970 <thecout970@gmail.com>
2018-02-13 03:33:11 +0100

    Updated to 2.1.0

--- src/main/kotlin/com/cout970/magneticraft/Magneticraft.kt
+++ src/main/kotlin/com/cout970/magneticraft/Magneticraft.kt
@@ -20,7 +20,7 @@ import kotlin.system.measureTimeMillis
 @Mod(
         modid = MOD_ID,
         name = MOD_NAME,
-        version = "2.0.5",
+        version = "2.1.0",
         modLanguage = "kotlin",
         modLanguageAdapter = LANG_ADAPTER,
         acceptedMinecraftVersions = "[1.12]",
