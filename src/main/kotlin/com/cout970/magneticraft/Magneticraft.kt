b946fb4959a14da72d88c4f6c1f03f45a691c8ca
cout970 <thecout970@gmail.com>
2018-08-16 15:50:14 +0200

    Hotfix 2.4.4

--- src/main/kotlin/com/cout970/magneticraft/Magneticraft.kt
+++ src/main/kotlin/com/cout970/magneticraft/Magneticraft.kt
@@ -22,11 +22,11 @@ import java.io.File
 @net.minecraftforge.fml.common.Mod(
         modid = MOD_ID,
         name = MOD_NAME,
-        version = "2.4.3",
+        version = "2.4.4",
         modLanguage = "kotlin",
         modLanguageAdapter = LANG_ADAPTER,
         acceptedMinecraftVersions = "[1.12]",
-        dependencies = "required-after:modelloader@[1.1.0,);required-after:forgelin",
+        dependencies = "required-after:modelloader@[1.1.1,);required-after:forgelin",
         updateJSON = "https://raw.githubusercontent.com/Magneticraft-Team/Magneticraft/1.12/src/main/resources/update.json"
 )
 //Singleton, see KotlinAdapter to know how it's loaded by forge
