3d998b9f479c283946cba4ce74ca80fc7d8c0cc0
cout970 <thecout970@gmail.com>
2018-09-29 18:16:43 +0200

    Join KotlinAdapter and Metadata.kt

--- src/main/kotlin/com/cout970/magneticraft/Metadata.kt
+++ src/main/kotlin/com/cout970/magneticraft/Metadata.kt
@@ -4,6 +4,12 @@ import com.cout970.magneticraft.misc.vector.Vec2d
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
 import net.minecraft.util.math.AxisAlignedBB
 import net.minecraft.util.math.Vec3d
+import net.minecraftforge.fml.common.FMLModContainer
+import net.minecraftforge.fml.common.ILanguageAdapter
+import net.minecraftforge.fml.common.ModContainer
+import net.minecraftforge.fml.relauncher.Side
+import java.lang.reflect.Field
+import java.lang.reflect.Method
 
 // Mod metadata used by Forge
 const val MOD_ID = "magneticraft"
@@ -16,3 +22,23 @@ typealias AABB = AxisAlignedBB
 typealias IVector3 = Vec3d
 typealias IVector2 = Vec2d
 typealias Sprite = TextureAtlasSprite
+
+/**
+ * This class allows to load the Mod class (Magneticraft.kt) into forge, this is needed because this
+ * class is an object (Singleton)
+ */
+@Suppress("unused")
+class KotlinAdapter : ILanguageAdapter {
+
+    override fun supportsStatics() = false
+
+    override fun setProxy(target: Field?, proxyTarget: Class<*>?, proxy: Any?) {
+        target?.set(null, proxy)
+    }
+
+    override fun getNewInstance(mod: FMLModContainer?, modClass: Class<*>?, loader: ClassLoader?, factory: Method?): Any? {
+        return modClass?.getField("INSTANCE")?.get(null)
+    }
+
+    override fun setInternalProxies(mod: ModContainer?, side: Side?, loader: ClassLoader?) = Unit
+}
