06d51393c34483bc98984c19fb9a2b3b19236de6
cout970 <thecout970@gmail.com>
2019-06-20 23:10:11 +0200

    Update 2.6.4

--- src/main/kotlin/com/cout970/magneticraft/Magneticraft.kt
+++ src/main/kotlin/com/cout970/magneticraft/Magneticraft.kt
@@ -22,7 +22,7 @@ import java.io.File
 @net.minecraftforge.fml.common.Mod(
     modid = MOD_ID,
     name = MOD_NAME,
-    version = "2.6.3",
+    version = "2.6.4",
     modLanguage = "kotlin",
     modLanguageAdapter = LANG_ADAPTER,
     acceptedMinecraftVersions = "[1.12]",
